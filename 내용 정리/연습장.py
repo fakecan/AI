26dac36e9f0cee0696f1020167c204a3e10cfb89
fakecan <52739724+fakecan@users.noreply.github.com>
2019-08-27 17:50:43 +0900

    commit

---
+++ 내용 정리/연습장.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x = np.array(range(10))
+print(x)
+
+arraySet = np.array(range(1, 101))
+print(arraySet)
+size = 7
+def split_n(seq, size):
+    newList = []
+    for i in range(len(seq) - size + 1): # i= 0~5 6줗
+        subset = seq[i:(i+size)] # 0~5 : 4~9 1줄씩
+        newList.append([item for item in subset])
+    # print(type(newList))    # list
+    return np.array(newList)
+
+dataset = split_n(arraySet, size)
+print(dataset)
+x_data = dataset[:, 0:6] #(6)
+y_data = dataset[:, -1] #(6, )
+print(x_data)
+print(y_data)
