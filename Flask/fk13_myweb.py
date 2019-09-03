89c43b57b7dcb4966481f196152392127501b6bf
fakecan <52739724+fakecan@users.noreply.github.com>
2019-09-03 21:46:56 +0900

    commit

--- Flask/fk13_myweb.py
+++ Flask/fk13_myweb.py
@@ -19,12 +19,15 @@ cursor = cnxn.cursor()
 
 # 커서에 쿼리를 입력해 실행시킵니다.
 tsql = "SELECT * FROM iris2;"
-with cursor.execute(tsql):
-    # 한 행을 가져옵니다
-    row = cursor.fetchone()
-    # 행이 존재할 때까지, 하나씩 행을 증가시키면서 모든 컬럼을 공백으로 구분해 출력합니다.
-    while row:
-        print(str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + " " +
-              str(row[3]) + " " + str(row[4]))
-        row = cursor.fetchone()
-cnxn.close()
+
+# flask 웹서버를 실행합니다.
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/sqltable')
+def showsql():
+    cursor.execute(tsql)
+    return render_template('myweb.html', rows=cursor.fetchall())
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=False)
---
+++ WordEmbedding/0903test.py
@@ -0,0 +1,4 @@
+from gensim.models.word2vec import Word2Vec
+import gensim
+
+path = ''
