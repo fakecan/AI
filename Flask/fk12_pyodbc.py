7cf4d30840cbab192d8439266db51ad17cdf612b
fakecan <52739724+fakecan@users.noreply.github.com>
2019-09-03 15:12:35 +0900

    Update fk12_pyodbc.py

--- Flask/fk12_pyodbc.py
+++ Flask/fk12_pyodbc.py
@@ -1,5 +1,5 @@
 # 모듈을 불러옵니다.
-import pymssql as ms
+import pyodbc as pyo
 
 # 연결 문자열을 설정합니다.
 server = 'localhost'
@@ -8,11 +8,11 @@ username = 'bit'
 password = '1234'
 
 # 데이터베이스를 연결합니다.
-cnxn = pyo.connect('DRIVERE={ODBC Driver 13 for SQL Server}; SERVER=' +server+
+cnxn = pyo.connect('DRIVER={ODBC Driver 13 for SQL Server}; SERVER=' +server+
                    '; PORT=1433; DATABASE=' +database+
                    ';UID=' +username+
                    ';PWD=' +password
-)
+                  )
 
 # 커서를 만듭니다.
 cursor = cnxn.cursor()
