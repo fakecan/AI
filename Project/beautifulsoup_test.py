be10b5f1841ac7df5e00c9942e0ce3f8ae834fda
fakecan <52739724+fakecan@users.noreply.github.com>
2019-09-17 21:20:40 +0900

    c

--- Project/beautifulsoup_test.py
+++ Project/beautifulsoup_test.py
@@ -12,3 +12,10 @@ path = "./Project/chromedriver.exe"
 driver = webdriver.Chrome(path)
 
 driver.get('https://weather.com/')
+sleep(1)
+
+user_ele = driver.find_element_by_class_name("theme__inputElement__4bZUj input__inputElement__1GjGE")
+user_ele.clear()
+user_ele.send_keys("Paris")
+
+logbtn = driver.find_element_by_css_selector("form input[type=']")
