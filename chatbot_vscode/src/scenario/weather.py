ae6e73a6604c0a838e347dd3b7330f7b7bf9f890
fakecan <52739724+fakecan@users.noreply.github.com>
2019-09-25 21:57:39 +0900

    Update weather.py

--- chatbot_vscode/src/scenario/weather.py
+++ chatbot_vscode/src/scenario/weather.py
@@ -7,21 +7,21 @@ import crawler.weather as crawler
 
 
 def weather(named_entity):
-    keyword_group = named_entity[0]
-    entity_group = named_entity[1]
+    keyword_group = named_entity[0] # ['내일', '광명', '날씨', '알려줘']
+    entity_group = named_entity[1]  # ['DATE', 'LOCATION', 'O', 'O']
     date = []
     location = []
 
-    for k in zip(keyword_group, entity_group):
-        if 'DATE' in k[1]:
+    for k in zip(keyword_group, entity_group):  # zip: 키워드 그룹과 엔티티 그룹을 같이 나열
+        if 'DATE' in k[1]:  # 날짜가 있으면 date에 저장
             date.append(k[0])
-        elif 'LOCATION' in k[1]:
+        elif 'LOCATION' in k[1]:    # 위치가 있으면 location에 저장
             location.append(k[0])
 
-    if len(date) == 0:
+    if len(date) == 0:  # 날짜 얘기가 없으면 '오늘'로 하겠다.
         date.append('오늘')
 
-    if len(location) == 0:
+    if len(location) == 0:  # 지역이 없으면 지역을 설정해주세요.
         while len(location) == 0:
             print('A.I : ' + '어떤 지역을 알려드릴까요?')
             print('User : ', end='', sep='')
@@ -29,7 +29,7 @@ def weather(named_entity):
             if loc is not None and loc.replace(' ', '') != '':
                 location.append(loc)
 
-    if '오늘' in date:
+    if '오늘' in date:  # 카테고리: 오늘, 내일, 모레, 내일모레, 이번주 등
         return crawler.today_weather(' '.join(location))
     elif date[0] == '내일':
         return crawler.tomorrow_weather(' '.join(location))
